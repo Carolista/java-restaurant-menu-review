a141606dc187209e1e071186b0ebaf4d513e408d
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-12 20:34:40 -0600

    coded solution during studio review

--- RestaurantMenu/src/main/java/restaurant/Menu.java
+++ RestaurantMenu/src/main/java/restaurant/Menu.java
@@ -1,4 +1,13 @@
 package restaurant;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Menu {
+
+    // TODO: Declare two private fields, menuItems (ArrayList<MenuItem>) and lastUpdated (Date)
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private Date lastUpdated;
+
+    // No constructor definition — allow for default constructor
 }
--- RestaurantMenu/src/main/java/restaurant/MenuItem.java
+++ RestaurantMenu/src/main/java/restaurant/MenuItem.java
@@ -1,4 +1,25 @@
 package restaurant;
 
+import java.util.Date;
+
 public class MenuItem {
+
+    // TODO: Declare private fields name, description, price, category, and dateAdded (Date)
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private Date dateAdded;
+
+    // TODO: Define constructor to take values for first four fields as params
+    // Initialize dateAdded to the current date
+    public MenuItem(String name, String description, double price, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.dateAdded = new Date();
+    }
+
 }
+
